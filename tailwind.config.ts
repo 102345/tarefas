20dd40da76349e640681c3f68da783adafc81202
102345 <marciliojcg@gmail.com>
2024-03-06 14:10:35 -0300

    Versão inicial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,38 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+// const config: Config = {
+  // content: [
+    // "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  // ],
+  // theme: {
+    // extend: {
+      // backgroundImage: theme => ({
+        // 'img-tarefas': "url('/imagens/background.png')"
+      // })
+    // },
+  // },
+  // plugins: [],
+// };
+// export default config;
+
+module.exports = {
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}'
   ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      backgroundImage: () => ({
+        'img-tarefas': "url('/images/background.png')"
+      })
     },
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-};
-export default config;
+}
+
